5b6892296fa4e2c18c01168591f208c2ea84810d
electricalbah <electricalbah@icloud.com>
2023-11-18 01:29:06 +0900

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,9 +1,12 @@
 #!/usr/bin/env groovy
 
+//call is special function in teh vars folder
+//called like - sayHello 'Alex'
 def call(String name = 'human') {
   echo "Hello, ${name}."
 }
 
+//called like -> sayHello.getWebsiteText("https://jsonplaceholder.typicode.com/todos/1")
 def getWebsiteText(url){
     return new URL(url)
           .getText(connectTimeout: 5000, 
@@ -13,13 +16,7 @@ def getWebsiteText(url){
                 requestProperties: ['Connection': 'close'])
 }
 
-def getjwtToken(guardrailurl,apikey){
-    body = """{\"apiKey\":\"${apikey}\"}"""   
-    req = httpRequest contentType: 'APPLICATION_JSON', httpMode: 'POST', quiet: true, ignoreSslErrors: true, requestBody: body, url: guardrailurl+"/v2/auth", wrapAsMultipart: false
-    jSon =  readJSON text: req.getContent()
-    JWT_TOKEN = jSon['jwtToken']
-    return JWT_TOKEN
-}
+
 
 
 
