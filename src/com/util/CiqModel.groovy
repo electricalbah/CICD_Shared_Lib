108043ec8a8211e8161354f76c6887c1f324ce44
electricalbah <electricalbah@icloud.com>
2023-11-18 17:51:57 +0900

    Update CiqModel.groovy

--- src/com/util/CiqModel.groovy
+++ src/com/util/CiqModel.groovy
@@ -1,8 +1,17 @@
-import groovy.transform.ToString
+//Maven POM to Groovy Grab Conversion
+//<!-- https://mvnrepository.com/artifact/net.sf.oval/oval -->
+//<dependency>
+//    <groupId>net.sf.oval</groupId>
+//    <artifactId>oval</artifactId>
+//    <version>1.31</version>
+//</dependency>
+@Grab(group='net.sf.oval', module='oval', version='1.31')   
 
 import net.sf.oval.constraint.MaxLength;
 import net.sf.oval.constraint.NotEmpty;
 import net.sf.oval.constraint.NotNull;
+
+import groovy.transform.ToString
 
 @ToString
 class CiqModel {
