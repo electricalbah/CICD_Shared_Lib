14d50cea1babb2b0bee9e8cd495bb7a93e8387d5
electricalbah <electricalbah@icloud.com>
2023-11-20 00:52:10 +0900

    Update ExcelUtil.groovy

--- src/com/util/ExcelUtil.groovy
+++ src/com/util/ExcelUtil.groovy
@@ -1,19 +1,21 @@
 #!/usr/bin/env groovy
 package com.util
 
-
 @Grab(group='org.apache.poi', module='poi', version='4.1.0')   // "group":"org.apache.poi", "id":"poi", "version":"3.8"
 @Grab(group='org.apache.poi', module='poi-ooxml', version='4.1.0') //"group":"org.apache.poi", "id":"poi-ooxml", "version":"3.8"
 // import org.apache.poi.ss.usermodel.Row;
 // import org.apache.poi.xssf.usermodel.XSSFSheet;
 // import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+//HSSFCell
 import org.apache.poi.ss.usermodel.*
 import org.apache.poi.ss.util.*
 import org.apache.poi.hssf.usermodel.*
 import org.apache.poi.xssf.usermodel.*
 import org.apache.poi.ss.usermodel.*
 
-//HSSFCell
+import net.sf.oval.ConstraintViolation
+import net.sf.oval.Validator
+
 
 
 class ExcelUtil {
@@ -43,6 +45,11 @@ def parseCIQFile(OutputDto outputDto){
               }
               ciqModel.setDescription(row.getCell(DESC).toString())
               ciqModel.setSystemTag(row.getCell(S_TAG).toString())
+              
+              //validate CIQ data
+              validateCIQRow(ciqModel, outputDto) 
+              
+              //set output DTO
               outputDto.ciqItems.put(row.getCell(PARAM).toString(), ciqModel)
                 //for (Cell cell : row) {
                   //echo "Cell Value: ${cell.toString()}"
@@ -57,6 +64,23 @@ def parseCIQFile(OutputDto outputDto){
 
   return outputDto
 }
+
+ def validateCIQRow(CiqModel ciqModel, OutputDto outputDto) {
+  // def excelCiqModel = new CiqModel("a", "b", "c", "d")              
+  Validator validator = new Validator();
+  List<ConstraintViolation> violations = validator.validate(ciqModel);
+  if(!violations.isEmpty()) {
+    print ">>>>> FAILURE"
+    outputDto.errors.add(violations.toString())
+    print violations.size()
+    print violations
+    //throw new Exception(violations);
+    print "BBBBBB"
+    } else {
+        print ">>>>> SUCCESS"
+        println ciqModel                    
+      } 
+ }
 
 //TODO - need clean up
 //https://poi.apache.org/apidocs/dev/org/apache/poi/ss/usermodel/CellType.html
